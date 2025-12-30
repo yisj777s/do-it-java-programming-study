5155943d581e069f93b07d813fca586213ebc339
yisj777s <yisj777s@naver.com>
2025-12-30 14:55:00 +0900

    feat: 02-3 문자 자료형(char) 예제 작성

---
+++ Chapter2/src/chapter2/CharacterEx1.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class CharacterEx1 {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        System.out.println(ch1); //문자 출력
+        System.out.println((int)ch1); //문자에 해당하는 정수값(아스키 코드값) 출력
+
+        char ch2 = 66; //정숫값 대입
+        System.out.println(ch2); //정수값에 해당하는 문자 출력
+
+        int ch3 = 67;
+        System.out.println(ch3); // 문자 정수값 출력
+        System.out.println((char)ch3); // 정수값에 해당하는 문자 출력
+    }
+}
