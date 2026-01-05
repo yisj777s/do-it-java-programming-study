c741cd2f47b99bab795213ad76df8b9fe6ae5731
yisj777s <yisj777s@naver.com>
2026-01-05 19:10:24 +0900

    feat: 02-3 논리형 변수 선언 및 출력 예제 작성

---
+++ Chapter2/src/chapter2/BooleanEX.java
@@ -0,0 +1,8 @@
+package chapter2;
+
+public class BooleanEX {
+    public static void main(String[] args){
+        boolean isMarried = true; //boolean 변수를 선언하고 초기화
+        System.out.println(isMarried);
+    }
+}
