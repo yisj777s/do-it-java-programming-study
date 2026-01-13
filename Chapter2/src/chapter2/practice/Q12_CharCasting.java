018d66efa51738b6bd0baa70ef2581ef5af9b275
yisj777s <yisj777s@naver.com>
2026-01-13 19:49:14 +0900

    feat(practice-02): Q12 문자형 형변환 출력

---
+++ Chapter2/src/chapter2/practice/Q12_CharCasting.java
@@ -0,0 +1,11 @@
+package chapter2.practice;
+
+public class Q12_CharCasting {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        System.out.println((int)ch1); // 문자에 해당하는 정숫값 출력
+
+        int ch2 = 67;
+        System.out.println((char)ch2); // 정숫값에 해당하는 문자 출력
+    }
+}
