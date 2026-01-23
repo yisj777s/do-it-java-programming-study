08552e716ba67047730dc617acd6f04c1f0accfa
yisj777s <yisj777s@naver.com>
2026-01-23 20:00:17 +0900

    feat(03-1): 복합 대입 연산자 예제 작성

---
+++ Chapter3/src/operator/CompoundAssignmentOperatorEx.java
@@ -0,0 +1,33 @@
+package operator;
+
+public class CompoundAssignmentOperatorEx {
+    public static void main(String[] args) {
+        int num = 10;
+
+        num += 2; // num = num + 2
+        System.out.println(num); // 12
+
+        num -= 2; // num = num - 2
+        System.out.println(num); // 10
+
+        num *= 2; // num = num * 2
+        System.out.println(num); // 20
+
+        num /= 2; // num = num / 2
+        System.out.println(num); // 10
+
+        num %= 2; // num = num % 2
+        System.out.println(num); // 0
+
+        num = 10; // 00001010
+
+        num <<= 2; // num = num << 2(00001010 << 2 → 00101000)
+        System.out.println(num); // 40
+
+        num >>= 2; // num = num >> 2(00101000 >> 2 → 00001010)
+        System.out.println(num); // 10
+
+        num >>>= 2; // num = num >>> 2
+        System.out.println(num);
+    }
+}
