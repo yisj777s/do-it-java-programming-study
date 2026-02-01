db6fcf3efe3304c9e0cee8ce0f7eae01a8ff242c
yisj777s <yisj777s@naver.com>
2026-02-01 22:11:48 +0900

    feat(04-1): if-else 조건문 예제 작성 (나이에 따라 다른 문장 출력하기)

---
+++ Chapter4/src/ifexample/IfExample1.java
@@ -0,0 +1,16 @@
+package ifexample;
+
+/**
+ * 나이에 따라 다른 문장 출력하기
+ */
+public class IfExample1 {
+    public static void main(String[] args) {
+        int age = 7;
+        if (age >= 8) {
+            System.out.println("학교에 다닙니다.");
+        }
+        else {
+            System.out.println("학교에 다니지 않습니다.");
+        }
+    }
+}
